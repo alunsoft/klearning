3cc23828852160cff08617d4dc1f009f63004492
alunsoft <alunsof@gmail.com>
2016-09-12 22:45:15 -0600

    Add connection

---
+++ phps/modulos.php
@@ -0,0 +1,40 @@
+<div class="x_title">
+  <h2>Tus Modulos</h2>
+  <div class="clearfix"></div>
+</div>
+<div class="x_content">
+  <div class="row">
+    <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+      <div class="tile-stats modulo modulo-primary" data-class="modulo-primary">
+        <div class="icon"><i class="fa fa-font text-primary"></i></div>
+        <div class="count">0</div>
+        <h3>A B C ...</h3>
+        <p>Alfabeto.</p>
+      </div>
+    </div>
+    <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+      <div class="tile-stats modulo modulo-info" data-class="modulo-info">
+        <div class="icon"><i class="fa fa-hashtag text-info"></i></div>
+        <div class="count">0</div>
+        <h3>1 2 3 ...</h3>
+        <p>Números.</p>
+      </div>
+    </div>
+    <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+      <div class="tile-stats modulo modulo-success" data-class="modulo-success">
+        <div class="icon"><i class="fa fa-users text-success"></i></div>
+        <div class="count">0</div>
+        <h3 class="text-success">Yo, tu, el ...</h3>
+        <p>Pronombres.</p>
+      </div>
+    </div>
+    <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+      <div class="tile-stats modulo modulo-warning" data-class="modulo-warning">
+        <div class="icon"><i class="fa fa-comments-o text-warning"></i></div>
+        <div class="count">0</div>
+        <h3 class="text-warning">ütz awäch</h3>
+        <p>Frases.</p>
+      </div>
+    </div>
+  </div>
+</div>
