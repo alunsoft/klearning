fc2070cfb3a6ddce8a2e0ba5798aec4c9f182264
alunsoft <alunsof@gmail.com>
2016-09-13 23:03:18 -0600

    chenge link

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if (!isset($_SESSION['id_usuario'])) {
           <div class="col-md-3 left_col">
             <div class="left_col scroll-view">
               <div class="navbar nav_title" style="border: 0;">
-                <a href="index.html" class="site_title"><i class="fa fa-book"></i> <span>K-Learning</span></a>
+                <a href="index.php" class="site_title"><i class="fa fa-book"></i> <span>K-Learning</span></a>
               </div>
 
               <div class="clearfix"></div>
